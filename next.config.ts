4a94dc1a2468b1f8c08396167212ee83f0ba53c4
Волга <aleksandrvolgin@MacBook-Air-Aleksandr.local>
2026-02-18 22:00:42 +0300

    fix: allow supabase images

--- next.config.ts
+++ next.config.ts
@@ -104,12 +104,19 @@ const nextConfig: NextConfig = {
     deviceSizes: [430, 640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rharjnqjchmbpwqrxgey.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
       { protocol: "https", hostname: "theame.ru", pathname: "/**" },
       { protocol: "http", hostname: "theame.ru", pathname: "/**" },
       ...supabaseHosts.map((host) => ({
         protocol: "https" as const,
         hostname: host,
-        pathname: "/**" as const,
+        port: "",
+        pathname: "/storage/v1/object/public/**" as const,
       })),
     ],
   },
